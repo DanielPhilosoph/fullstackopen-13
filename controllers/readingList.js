021fcaba76c3fa3bc0d55f37b03d0548ef736237
DanielPhilosoph <danielph9@gmail.com>
2022-01-15 18:55:06 +0200

    13.22

--- controllers/readingList.js
+++ controllers/readingList.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
-const { User, Blogs } = require("../models");
+const { Op } = require("sequelize");
+const { User, Blogs, ReadingList } = require("../models");
 const { tokenExtractor } = require("../util/middleware");
 
 router.post("/", tokenExtractor, async (req, res) => {
@@ -8,10 +9,6 @@ router.post("/", tokenExtractor, async (req, res) => {
     try {
       const user = await User.findByPk(req.body.user_id);
       const blog = await Blogs.findByPk(req.body.blog_id);
-      console.log("User:");
-      console.log(user);
-      console.log("Blog:");
-      console.log(blog);
       await user.addBlogs(blog, { through: { have_read: true } });
       //await blog.addUser(user, { through: { have_read: true } });
       return res.send("yay");
@@ -21,6 +18,22 @@ router.post("/", tokenExtractor, async (req, res) => {
     }
   } else {
     return res.status(400).json({ error: "Missing user_id / blog_id in body" });
+  }
+});
+
+router.put("/:id", tokenExtractor, async (req, res) => {
+  if (req.body.read !== undefined) {
+    const user_id = req.decodedToken.id;
+    const readingList = await ReadingList.findAll({
+      where: { [Op.and]: { id: req.params.id, userId: user_id } },
+    });
+    if (readingList.length !== 0) {
+      readingList[0].have_read = req.body.read;
+      await readingList[0].save();
+      res.json({ status: "Updated", readingList: readingList[0] });
+    }
+  } else {
+    return res.status(400).json({ error: "Missing read in body" });
   }
 });
 
